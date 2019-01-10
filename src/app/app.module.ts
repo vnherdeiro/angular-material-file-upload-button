726c2ec1a41eb769393b96612258dbd00aaa390e
Victor Herdeiro <victor.oriedreh@gmail.com>
2019-01-10 14:57:00 +0000

    cleaned README.md

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+
+import { MatButtonModule,
+	MatIconModule,
+}
+from '@angular/material';
+import { UploadButtonComponent } from './upload-button/upload-button.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+	AppComponent,
+	UploadButtonComponent
+	],
+	imports: [
+	BrowserModule,
+	HttpClientModule,
+	BrowserAnimationsModule,
+	MatButtonModule,
+	MatIconModule,
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
